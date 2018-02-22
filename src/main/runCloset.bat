dfaca4138144d36a917de8aa8d6924f87edf81df
Kyle <kdarling95@yahoo.com>
2018-02-22 17:35:41 -0500

    Batch script updated to run project outside of Intellij or via Terminal inside the program.
    A new message

--- src/main/runCloset.bat
+++ src/main/runCloset.bat
@@ -1,3 +1,5 @@
+echo off
+
 javac -d classes -classpath classes; common\*.java database\*.java event\*.java exception\*.java impresario\*.java model\*.java userinterface\*.java Utilities\*.java
 javac -classpath classes;. Closet.java
 java -cp mysql-connector-java-5.1.7-bin.jar;classes;. Closet
