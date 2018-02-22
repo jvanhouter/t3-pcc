1a3eb5dd1c41cebbd37bf7ef05e9792a1175d109
Jackson <jr_taber@yahoo.com>
2018-02-22 17:54:41 -0500

    imports

--- src/main/model/Color.java
+++ src/main/model/Color.java
@@ -1,4 +1,24 @@
 package model;
 
+// system imports
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Vector;
+import javax.swing.JFrame;
+
+// project imports
+import exception.InvalidPrimaryKeyException;
+import database.*;
+
+import impresario.IView;
+
+/**
+ * Jan 30, 2018
+ * @author Jackson Taber & Kyle Darling
+ */
+
 public class Color {
+
+    Color(){}
 }
