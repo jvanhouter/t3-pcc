7ae413fe76b9e25be997bf00a3fe2811bad140a0
Erik Nystrom <erik.nystrom87@gmail.com>
2018-02-28 22:03:04 -0500

    Issue #5, adding relevant ClothingItem files, init

---
+++ src/main/model/AddClothingItemTransaction.java
@@ -0,0 +1,173 @@
+// specify the package
+package model;
+
+// system imports
+
+import event.Event;
+import exception.InvalidPrimaryKeyException;
+import exception.MultiplePrimaryKeysException;
+import javafx.scene.Scene;
+import userinterface.View;
+import userinterface.ViewFactory;
+
+import java.util.Properties;
+
+// project imports
+
+/** The class containing the AddArticleTypeTransaction for the Professional Clothes Closet application */
+//==============================================================
+public class AddClothingItemTransaction extends Transaction
+{
+
+	private ArticleType myArticleType;
+
+
+	// GUI Components
+
+	private String transactionErrorMessage = "";
+
+	/**
+	 * Constructor for this class.
+	 *
+	 */
+	//----------------------------------------------------------
+	public AddClothingItemTransaction() throws Exception
+	{
+		super();
+	}
+
+	//----------------------------------------------------------
+	protected void setDependencies()
+	{
+		dependencies = new Properties();
+		dependencies.setProperty("CancelAddClothingItem", "CancelTransaction");
+		dependencies.setProperty("OK", "CancelTransaction");
+		dependencies.setProperty("ClothingItemData", "TransactionError");
+
+		myRegistry.setDependencies(dependencies);
+	}
+
+	/**
+	 * This method encapsulates all the logic of creating the article type,
+	 * verifying its uniqueness, etc.
+	 */
+	//----------------------------------------------------------
+	public void processTransaction(Properties props)
+	{
+		if (props.getProperty("BarcodePrefix") != null)
+		{
+			String barcodePrefix = props.getProperty("BarcodePrefix");
+			try
+			{
+
+				ArticleType oldArticleType = new ArticleType(barcodePrefix);
+				transactionErrorMessage = "ERROR: Barcode Prefix " + barcodePrefix 
+					+ " already exists!";
+				new Event(Event.getLeafLevelClassName(this), "processTransaction",
+						"Article type with barcode prefix : " + barcodePrefix + " already exists!",
+						Event.ERROR);
+			}
+			catch (InvalidPrimaryKeyException ex)
+			{
+				// Barcode prefix does not exist, validate data
+				try
+				{
+					int barcodePrefixVal = Integer.parseInt(barcodePrefix);
+					String descriptionOfAT = props.getProperty("Description");
+					if (descriptionOfAT.length() > 30)
+					{
+						transactionErrorMessage = "ERROR: Article Type Description too long! ";
+					}
+					else
+					{
+						String alphaCode = props.getProperty("AlphaCode");
+						if (alphaCode.length() > 5)
+						{
+							transactionErrorMessage = "ERROR: Alpha code too long (max length = 5)! ";
+						}
+						else
+						{
+								props.setProperty("Status", "Active");
+								myArticleType = new ArticleType(props);
+								myArticleType.update();
+								transactionErrorMessage = (String)myArticleType.getState("UpdateStatusMessage");
+						}
+					}
+				}
+				catch (Exception excep)
+				{
+					transactionErrorMessage = "ERROR: Invalid barcode prefix: " + barcodePrefix 
+						+ "! Must be numerical.";
+					new Event(Event.getLeafLevelClassName(this), "processTransaction",
+						"Invalid barcode prefix : " + barcodePrefix + "! Must be numerical.",
+						Event.ERROR);
+				}
+
+			}
+			catch (MultiplePrimaryKeysException ex2)
+			{
+				transactionErrorMessage = "ERROR: Multiple article types with barcode prefix!";
+				new Event(Event.getLeafLevelClassName(this), "processTransaction",
+					"Found multiple article types with barcode prefix : " + barcodePrefix + ". Reason: " + ex2.toString(),
+					Event.ERROR);
+
+			}
+		}
+		
+	}
+
+	//-----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("TransactionError"))
+		{
+			return transactionErrorMessage;
+		}
+		
+		
+		return null;
+	}
+
+	//-----------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+		// DEBUG System.out.println("AddArticleTypeTransaction.sCR: key: " + key);
+
+		if (key.equals("DoYourJob"))
+		{
+			doYourJob();
+		}
+		else
+		if (key.equals("ClothingItemData"))
+		{
+			processTransaction((Properties)value);
+		}
+
+		myRegistry.updateSubscribers(key, this);
+	}
+
+	/**
+	 * Create the view of this class. And then the super-class calls
+	 * swapToView() to display the view in the frame
+	 */
+	//------------------------------------------------------
+	protected Scene createView()
+	{
+		Scene currentScene = myViews.get("AddClothingItemView");
+
+		if (currentScene == null)
+		{
+			// create our initial view
+			currentScene = new Scene(ViewFactory.createView("AddClothingItemView", this));
+			myViews.put("AddClothingItemView", currentScene);
+
+			return currentScene;
+		}
+		else
+		{
+			return currentScene;
+		}
+	}
+
+}
+
--- src/main/model/TransactionFactory.java
+++ src/main/model/TransactionFactory.java
@@ -1,53 +1,23 @@
 // specify the package
 package model;
 
-// system imports
-import java.util.Vector;
-
-
-// project imports
-
-/** The class containing the TransactionFactory for the Professional Clothes 
- *  Closet application 
+/**
+ * The class containing the TransactionFactory for the Professional Clothes
+ * Closet application
  */
 //==============================================================
-public class TransactionFactory
-{
+public class TransactionFactory {
+    public static Transaction createTransaction(String transType)
+            throws Exception {
 
-	/**
-	 *
-	 */
-	//----------------------------------------------------------
-	public static Transaction createTransaction(String transType)
-		throws Exception
-	{
-		Transaction retValue = null;
-
-		if (transType.equals("AddArticleType") == true)
-		{
-			retValue = new AddArticleTypeTransaction();
-		} 
-		else
-		if (transType.equals("UpdateArticleType") == true)
-		{
-			retValue = new UpdateArticleTypeTransaction();
-		}
-		else
-		if (transType.equals("CheckoutClothingItem") == true)
-		{
-			retValue = new CheckoutClothingItemTransaction();
-		}/*
-		else
-		if (transType.equals("BalanceInquiry") == true)
-		{
-			retValue = new BalanceInquiryTransaction(cust);
-		}
-		else
-		if (transType.equals("ImposeServiceCharge") == true)
-		{
-			retValue = new ImposeServiceChargeTransaction(cust);
-		} */
-
-		return retValue;
-	}
+        if (transType.equals("AddArticleType")) {
+            return new AddArticleTypeTransaction();
+        } else if (transType.equals("UpdateArticleType")) {
+            return new UpdateArticleTypeTransaction();
+        } else if (transType.equals("CheckoutClothingItem")) {
+            return new CheckoutClothingItemTransaction();
+        } else {
+            return null;
+        }
+    }
 }
