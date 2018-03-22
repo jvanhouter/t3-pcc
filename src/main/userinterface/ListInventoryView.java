359cbb309d2fd4098542895066052d3a081af1f3
The Alalah <leinadalaya@yahoo.com>
2018-03-22 18:07:14 -0400

    ListInventoryView, unexplained errors(maybe imports?) ListInventoryCollection, getState() keys need to be changed InventoryTableModel, imports? unused methods?

--- src/main/userinterface/ListInventoryView.java
+++ src/main/userinterface/ListInventoryView.java
@@ -37,12 +37,13 @@ import java.util.Enumeration;
 
 // project imports
 import impresario.IModel;
-import model.ListInventoryCollection;
+//import model.Item;
+//import model.ListInvenotryCollection;
 
 //==============================================================================
 public class ListInventoryView extends View
 {
-    protected TableView<InventoryTableModel> tableOfAccounts;
+    protected TableView<InventoryTableModel> tableOfInventory;
     protected Button cancelButton;
     protected Button submitButton;
 
@@ -50,9 +51,9 @@ public class ListInventoryView extends View
 
 
     //--------------------------------------------------------------------------
-    public ListInventoryView(IModel wsc)
+    public ListInventoryView(IModel matt)
     {
-        super(wsc, "ListInventoryView");
+        super(matt, "ListInventoryView");
 
         // create a container for showing the contents
         VBox container = new VBox(10);
@@ -83,20 +84,29 @@ public class ListInventoryView extends View
         ObservableList<InventoryTableModel> tableData = FXCollections.observableArrayList();
         try
         {
-            ClothingItemCollection itemCollection = (ClothingItemCollection)myModel.getState("AccountList");
+            ListInventoryCollection listInvenotryCollection =
+                    (ListInvenotryCollection)myModel.getState("InventoryList");
 
-            Vector entryList = (Vector)itemCollection.getState("Accounts");
-            Enumeration entries = entryList.elements();
+            Vector entryList = (Vector)listInvenotryCollection.getState("Inventory");
 
-            while (entries.hasMoreElements() == true)
+            if (entryList.size() > 0)
             {
-                Item nextAccount = (Item)entries.nextElement();
-                Vector<String> view = nextItem.getEntryListView();
+                Enumeration entries = entryList.elements();
 
-                // add this list entry to the list
-                InventoryTableModel nextTableRowData = new InventoryTableModel(view);
-                tableData.add(nextTableRowData);
+                while (entries.hasMoreElements() == true)
+                {
+                    Item nextAT = (Item)entries.nextElement();
+                    Vector<String> view = nextAT.getEntryListView();
 
+                    // add this list entry to the list
+                    InventoryTableModel nextTableRowData = new InventoryTableModel(view);
+                    tableData.add(nextTableRowData);
+
+                }
+            }
+            else
+            {
+                displayMessage("No matching entries found!");
             }
 
             tableOfInventory.setItems(tableData);
@@ -110,15 +120,43 @@ public class ListInventoryView extends View
     //-------------------------------------------------------------
     private Node createTitle()
     {
-        HBox container = new HBox();
-        container.setAlignment(Pos.CENTER);
+        VBox container = new VBox(10);
+        container.setPadding(new Insets(1, 1, 1, 30));
 
-        Text titleText = new Text(" Brockport Bank ATM ");
+        Text clientText = new Text(" Office of Career Services ");
+        clientText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        clientText.setWrappingWidth(350);
+        clientText.setTextAlignment(TextAlignment.CENTER);
+        clientText.setFill(Color.DARKGREEN);
+        container.getChildren().add(clientText);
+
+        Text collegeText = new Text(" THE COLLEGE AT BROCKPORT ");
+        collegeText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
+        collegeText.setWrappingWidth(350);
+        collegeText.setTextAlignment(TextAlignment.CENTER);
+        collegeText.setFill(Color.DARKGREEN);
+        container.getChildren().add(collegeText);
+
+        Text titleText = new Text(" Professional Clothes Closet Management System ");
         titleText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        titleText.setWrappingWidth(300);
+        titleText.setWrappingWidth(350);
         titleText.setTextAlignment(TextAlignment.CENTER);
         titleText.setFill(Color.DARKGREEN);
         container.getChildren().add(titleText);
+
+        Text blankText = new Text("  ");
+        blankText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        blankText.setWrappingWidth(350);
+        blankText.setTextAlignment(TextAlignment.CENTER);
+        blankText.setFill(Color.WHITE);
+        container.getChildren().add(blankText);
+
+        Text actionText = new Text("      ** Inventory Listing **       ");
+        actionText.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        actionText.setWrappingWidth(350);
+        actionText.setTextAlignment(TextAlignment.CENTER);
+        actionText.setFill(Color.BLACK);
+        container.getChildren().add(actionText);
 
         return container;
     }
@@ -129,121 +167,162 @@ public class ListInventoryView extends View
     {
         VBox vbox = new VBox(10);
 
+        Text prompt = new Text("");
+        prompt.setWrappingWidth(400);
+        prompt.setTextAlignment(TextAlignment.CENTER);
+        prompt.setFill(Color.BLACK);
+        prompt.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        vbox.getChildren().add(prompt);
+
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-
-        Text prompt = new Text("List Of Items");
-        prompt.setWrappingWidth(350);
-        prompt.setTextAlignment(TextAlignment.CENTER);
-        prompt.setFill(Color.BLACK);
-        grid.add(prompt, 0, 0, 2, 1);
+        grid.setPadding(new Insets(0, 25, 10, 0));
 
         tableOfInventory = new TableView<InventoryTableModel>();
         tableOfInventory.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-        TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));
-        TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
-                new PropertyValueFactory<InventoryTableModel, String>("status"));TableColumn accountNumberColumn = new TableColumn("Account Number") ;
-        accountNumberColumn.setMinWidth(100);
-        accountNumberColumn.setCellValueFactory(
+        TableColumn barcodeColumn = new TableColumn("Barcode") ;
+        barcodeColumn.setMinWidth(50);
+        barcodeColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("barcode"));
+
+        TableColumn genderColumn = new TableColumn("Gender") ;
+        genderColumn.setMinWidth(150);
+        genderColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("gender"));
+
+        TableColumn sizeColumn = new TableColumn("Size") ;
+        sizeColumn.setMinWidth(50);
+        sizeColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("size"));
+
+        TableColumn articleTypeColumn = new TableColumn("Article Type") ;
+        articleTypeColumn.setMinWidth(50);
+        articleTypeColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("articleType"));
+
+        TableColumn color1Column = new TableColumn("Color1") ;
+        color1Column.setMinWidth(50);
+        color1Column.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("color1"));
+
+        TableColumn color2Column = new TableColumn("Color2") ;
+        color2Column.setMinWidth(50);
+        color2Column.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("color2"));
+
+        TableColumn brandColumn = new TableColumn("Brand") ;
+        brandColumn.setMinWidth(50);
+        brandColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("brand"));
+
+        TableColumn notesColumn = new TableColumn("Notes") ;
+        notesColumn.setMinWidth(50);
+        notesColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("notes"));
+
+        TableColumn statusColumn = new TableColumn("Status") ;
+        statusColumn.setMinWidth(50);
+        statusColumn.setCellValueFactory(
                 new PropertyValueFactory<InventoryTableModel, String>("status"));
 
-        tableOfInventory.getColumns().addAll(accountNumberColumn,
-                accountTypeColumn, balanceColumn, serviceChargeColumn);
+        TableColumn donorFirstNameColumn = new TableColumn("Donor First Name") ;
+        donorFirstNameColumn.setMinWidth(50);
+        donorFirstNameColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("donorFirstName"));
 
-//        tableOfAccounts.setOnMousePressed(new EventHandler<MouseEvent>() {
+        TableColumn donorLastNameColumn = new TableColumn("Donor Last Name") ;
+        donorLastNameColumn.setMinWidth(50);
+        donorLastNameColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("donorLastName"));
+
+        TableColumn donorPhoneColumn = new TableColumn("Donor Phone") ;
+        donorPhoneColumn.setMinWidth(50);
+        donorPhoneColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("donorPhone"));
+
+        TableColumn donorEmailColumn = new TableColumn("Donor Email") ;
+        donorEmailColumn.setMinWidth(50);
+        donorEmailColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("donorEmail"));
+
+        TableColumn receiverNetIdColumn = new TableColumn("Receiver Net ID") ;
+        receiverNetIdColumn.setMinWidth(50);
+        receiverNetIdColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("receiverNetId"));
+
+        TableColumn receiverFirstNameColumn = new TableColumn("Receiver First Name") ;
+        receiverFirstNameColumn.setMinWidth(50);
+        receiverFirstNameColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("receiverFirstName"));
+
+        TableColumn receiverLastNameColumn = new TableColumn("Receiver Last Name") ;
+        receiverLastNameColumn.setMinWidth(50);
+        articleTypeColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("receiverLastName"));
+
+        TableColumn dateDonatedColumn = new TableColumn("Date Donated") ;
+        dateDonatedColumn.setMinWidth(50);
+        dateDonatedColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("dateDonated"));
+
+        TableColumn dateTakenColumn = new TableColumn("Date Taken") ;
+        dateTakenColumn.setMinWidth(50);
+        dateTakenColumn.setCellValueFactory(
+                new PropertyValueFactory<InventoryTableModel, String>("dateTaken"));
+
+
+        tableOfInventory.getColumns().addAll(barcodeColumn, genderColumn, sizeColumn,
+                articleTypeColumn, color1Column, color2Column, brandColumn, notesColumn, statusColumn,
+                donorLastNameColumn, donorFirstNameColumn, donorPhoneColumn, donorEmailColumn,
+                receiverNetIdColumn, receiverLastNameColumn, receiverFirstNameColumn,
+                dateDonatedColumn, dateTakenColumn);
+
+//        tableOfInventory.setOnMousePressed(new EventHandler<MouseEvent>() {
 //            @Override
 //            public void handle(MouseEvent event)
 //            {
 //                if (event.isPrimaryButtonDown() && event.getClickCount() >=2 ){
-//                    processAccountSelected();
+//                    processArticleTypeSelected();
 //                }
 //            }
 //        });
+
         ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setPrefSize(115, 150);
+        scrollPane.setPrefSize(150, 150);
         scrollPane.setContent(tableOfInventory);
 
-        submitButton = new Button("Submit");
+//        submitButton = new Button("Submit");
+//        submitButton.setFont(Font.font("Arial", FontWeight.BOLD, 14));
 //        submitButton.setOnAction(new EventHandler<ActionEvent>() {
 //
 //            @Override
 //            public void handle(ActionEvent e) {
 //                clearErrorMessage();
 //                // do the inquiry
-//                processAccountSelected();
+//                processArticleTypeSelected();
 //
 //            }
 //        });
 
-        cancelButton = new Button("Back");
+        cancelButton = new Button("Return");
+        cancelButton.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         cancelButton.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent e) {
                 /**
                  * Process the Cancel button.
-                 * The ultimate result of this action is that the transaction will tell the teller to
-                 * to switch to the transaction choice view. BUT THAT IS NOT THIS VIEW'S CONCERN.
+                 * The ultimate result of this action is that the transaction will tell the Receptionist to
+                 * to switch to the Receptionist view. BUT THAT IS NOT THIS VIEW'S CONCERN.
                  * It simply tells its model (controller) that the transaction was canceled, and leaves it
-                 * to the model to decide to tell the teller to do the switch back.
+                 * to the model to decide to tell the Receptionist to do the switch back.
                  */
                 //----------------------------------------------------------
                 clearErrorMessage();
-                myModel.stateChangeRequest("CancelAccountList", null);
+                myModel.stateChangeRequest("CancelInventoryList", null);
             }
         });
 
@@ -259,23 +338,21 @@ public class ListInventoryView extends View
         return vbox;
     }
 
-
-
     //--------------------------------------------------------------------------
     public void updateState(String key, Object value)
     {
     }
 
     //--------------------------------------------------------------------------
-//    protected void processAccountSelected()
+//    protected void processArticleTypeSelected()
 //    {
-//        InventoryTableModel selectedItem = tableOfAccounts.getSelectionModel().getSelectedItem();
+//        InventoryTableModel selectedItem = tableOfInventory.getSelectionModel().getSelectedItem();
 //
 //        if(selectedItem != null)
 //        {
-//            String selectedAcctNumber = selectedItem.getAccountNumber();
+//            String selectedBarcodePrefix = selectedItem.getBarcodePrefix();
 //
-//            myModel.stateChangeRequest("AccountSelected", selectedAcctNumber);
+//            myModel.stateChangeRequest("ArticleTypeSelected", selectedBarcodePrefix);
 //        }
 //    }
 
@@ -305,15 +382,6 @@ public class ListInventoryView extends View
     {
         statusLog.clearErrorMessage();
     }
-	/*
-	//--------------------------------------------------------------------------
-	public void mouseClicked(MouseEvent click)
-	{
-		if(click.getClickCount() >= 2)
-		{
-			processAccountSelected();
-		}
-	}
-   */
+
 
 }
