cd1b8ed3c5d7a4c1067cd432b46708095778d44f
The Alalah <leinadalaya@yahoo.com>
2018-04-03 18:16:06 -0400

    Inventory working

--- src/main/model/InventoryItemCollection.java
+++ src/main/model/InventoryItemCollection.java
@@ -93,7 +93,7 @@ public class InventoryItemCollection  extends EntityBase implements IView
     //-----------------------------------------------------------
     public void findAll()
     {
-        String query = "SELECT * FROM " + myTableName + " WHERE (Status = 'Received')";
+        String query = "SELECT * FROM " + myTableName + " WHERE (Status = 'Donated')";
         populateCollectionHelper(query);
     }
 
