79c7f1a5bb2ef8f0612d1b71df46622c7943d3e4
Erik Nystrom <erik.nystrom87@gmail.com>
2018-04-19 01:11:16 -0400

    new button stylings for click/hover

--- src/main/userinterface/PccButton.java
+++ src/main/userinterface/PccButton.java
@@ -15,16 +15,20 @@ class PccButton extends Button {
         this.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         this.setStyle("-fx-border-color: #ffc726; -fx-border-width: 1px; -fx-background-color: #00533e; -fx-text-fill: #ffc726");
         this.setOnMouseEntered(e -> {
-            grow();
+//            grow();
+            hover();
         });
         this.setOnMouseExited(e -> {
-            normal();
+            reset();
+//            normal();
         });
         this.setOnMouseReleased(e -> {
-            grow();
+//            grow();
+            reset();
         });
         this.setOnMousePressed(e -> {
-            shrink();
+//            shrink();
+            clicked();
         });
 
     }
@@ -43,5 +47,13 @@ class PccButton extends Button {
         this.setScaleX(1.0);
         this.setScaleY(1.0);
     }
-
+    private void clicked(){
+        this.setStyle("-fx-border-color: #cc9402; -fx-border-width: 1px; -fx-background-color: #00200b; -fx-text-fill: #eeb615");
+    }
+    private void hover() {
+        this.setStyle("-fx-border-color: #eeb615; -fx-border-width: 1px; -fx-background-color: #00422d; -fx-text-fill: #eeb615");
+    }
+    private void reset() {
+        this.setStyle("-fx-border-color: #ffc726; -fx-border-width: 1px; -fx-background-color: #00533e; -fx-text-fill: #ffc726");
+    }
 }
